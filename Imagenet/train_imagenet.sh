215e21fc5fb3096abb58b2918f4412d460f27d88
Adam Swart <swartadam@gmail.com>
2016-10-24 22:41:12 +0200

    training imagenet

--- Imagenet/train_imagenet.sh
+++ Imagenet/train_imagenet.sh
@@ -4,6 +4,7 @@ TOOLS=~/bin/caffe-master/build/tools
 
 $TOOLS/caffe train \
   -gpu 0 \
-  -solver solver.prototxt 2>&1 | tee log/imagenet_log.log
+  -solver solver.prototxt \
+  -snapshot adamnet_train_iter_60000.solverstate 2>&1 | tee log/imagenet_log.log
 
 python ~/bin/caffe-master/tools/extra/parse_log.py log/imagenet_log.log .
